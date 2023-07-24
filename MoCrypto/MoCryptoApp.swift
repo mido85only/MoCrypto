7bf894e170274b215099aab2ac72c9df37cb5944
Mohamed Aziz <aziz@corptia.com>
2023-07-24 10:46:28 +0300

    Initial Commit

---
+++ MoCrypto/MoCryptoApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoCryptoApp.swift
+//  MoCrypto
+//
+//  Created by Corptia on 24/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct MoCryptoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
